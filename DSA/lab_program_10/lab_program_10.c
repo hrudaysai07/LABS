99167ca601cb3e2451ccc737021ec4c1b2826fa7
Duggandla Hruday Sai Reddy <duggandla.cs24@bmsce.ac.in>
2025-12-30 16:20:14 +0530

    Create lab_program_10.c

---
+++ DSA/lab_program_10/lab_program_10.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAX 20
+
+int hashTable[MAX];
+int m;
+void init() {
+    for (int i = 0; i < m; i++) {
+        hashTable[i] = -1;
+    }
+}
+void insert(int key) {
+    int index = key % m;
+
+    if (hashTable[index] == -1) {
+        hashTable[index] = key;
+    } else {
+        int i = 1;
+        while (hashTable[(index + i) % m] != -1) {
+            i++;
+        }
+        hashTable[(index + i) % m] = key;
+    }
+}
+void display() {
+    printf("\nHash Table:\n");
+    for (int i = 0; i < m; i++) {
+        if (hashTable[i] == -1)
+            printf("HT[%d] : EMPTY\n", i);
+        else
+            printf("HT[%d] : %d\n", i, hashTable[i]);
+    }
+}
+
+int main() {
+    int n, key;
+    printf("Enter size of hash table (m): ");
+    scanf("%d", &m);
+    init();
+    printf("Enter number of employee records: ");
+    scanf("%d", &n);
+    printf("Enter %d employee keys (4-digit):\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &key);
+        insert(key);
+    }
+    display();
+    return 0;
+}
